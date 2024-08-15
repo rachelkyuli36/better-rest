3ba3b833ee06624e8b381986ff04ad6317c3421d
rachelkyuli36 <78241037+rachelkyuli36@users.noreply.github.com>
2024-08-15 10:42:14 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rachel Chung on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
